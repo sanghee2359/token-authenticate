d6122b3e2c12763e499ac78c6a22070f660178a7
sanghee <wjdtkdgml7352@daum.net>
2022-12-06 16:40:16 +0900

    [추가] JwtFilter Block - 1 - token이 없으면 권한부여하지 않기

--- src/main/java/com/token/authenticate/configuration/JwtTokenFilter.java
+++ src/main/java/com/token/authenticate/configuration/JwtTokenFilter.java
@@ -3,6 +3,7 @@ package com.token.authenticate.configuration;
 import com.token.authenticate.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.http.HttpHeaders;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,8 +26,19 @@ public class JwtTokenFilter extends OncePerRequestFilter { // 한번요청할때
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-//        log.info("authorizationHeader:{}", authorizationHeader);
+        // header에서 token 꺼내기
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader:{}", authorizationHeader);
+
+        // header이 존재하지 않을 경우 block
+        if(authorizationHeader == null) {
+            log.error("authorizationHeader이 없습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+
         // 문열어주기
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("",null, List.of(new SimpleGrantedAuthority("USER")));
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
